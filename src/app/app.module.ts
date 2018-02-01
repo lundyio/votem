f4684a92f03c23e215062ce9e6d3947df07c1554
Lundy <lundy@lundy.io>
2018-02-01 12:04:23 -0500

    adding page leve components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { VoteComponent } from './components/vote/vote.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'vote', component: VoteComponent },
+  { path: 'admin', component: AdminComponent },
+  //{ path: 'vote/:id', component: ArticleComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    VoteComponent,
+    LandingComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
