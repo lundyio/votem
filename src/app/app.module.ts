a6a238f1fe6e37635da450c12ba0e8e2e963e23b
Lundy <lundy@lundy.io>
2018-02-01 14:25:46 -0500

    sticky footer, fixing window height issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,17 @@ import { LandingComponent } from './components/landing/landing.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 const appRoutes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'vote', component: VoteComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'admin/login', component: LoginComponent },
   //{ path: 'vote/:id', component: ArticleComponent },
   { path: '404', component: PageNotFoundComponent },
   { path: '',   redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'login',   redirectTo: 'admin/login', pathMatch: 'full' },
   { path: '**', redirectTo: '404' }
 ];
 
@@ -28,7 +31,8 @@ const appRoutes: Routes = [
     LandingComponent,
     PageNotFoundComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  sticky: boolean = false;
+  appRoot: any;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.appRoot = document.getElementsByTagName('app-root')[0];
+
+    if( this.appRoot.offsetHeight > window.innerHeight){
+      this.sticky = false;
+    } else {
+      this.sticky = true;
+    }
+
   }
 
 }
