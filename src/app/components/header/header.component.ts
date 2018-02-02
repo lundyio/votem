155f5be610999f55575b86a4b527c5c1c3d8ace3
Lundy <lundy@lundy.io>
2018-02-02 13:43:25 -0500

    login page styles and form functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input('parent') parent: string;
+  showLoginButton: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+
+    if(this.parent === 'login'){
+      this.showLoginButton = false;
+    } else {
+      this.showLoginButton = true;
+    }
+
   }
 
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  tooltip: boolean = false;
+  invalid: boolean = false;
+
   constructor() { }
 
+  onSubmit(f){
+    if(f.invalid){
+      this.invalid = true;
+    } else if(f.value.username === 'admin' && f.value.password === 'password') {
+      this.invalid = false;
+    } else {
+      this.invalid = false;
+      window.setTimeout(() => { this.invalid = true; });
+    }
+  }
+
   ngOnInit() {
+
   }
 
 }
